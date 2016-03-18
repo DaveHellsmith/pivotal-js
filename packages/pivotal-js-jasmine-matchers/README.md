d714b3dd2de1df4df47c7a0583a675a40fef9906
Elena Sharma <esharma@pivotal.io>
2016-03-18 16:07:41 -0700

    Publish task adds authors
    * added jasmine-matchers README
    * added LICENSE
    
    Signed-off-by: Charles Hansen <chansen@pivotal.io>

--- packages/pivotal-js-jasmine-matchers/README.md
+++ packages/pivotal-js-jasmine-matchers/README.md
@@ -1,1 +1,35 @@
-TODO
+## Jasmine Matchers
+
+```sh
+npm install pivotal-js-jasmine-matchers --save-dev
+```
+
+In your spec helper,
+
+```js
+require('pivotal-js-jasmine-matchers');
+```
+
+Jasmine matcher libraries
+
+### Request Matchers
+
+For use with `jasmine-ajax`
+
+* toHaveBeenRequested
+  `expect('run.pivotal.io').toHaveBeenRequested();`
+* toHaveBeenRequestedWithQuery
+  `expect('run.pivotal.io').toHaveBeenRequestWithQuery({page: 2, perPage: 50});`
+* toHaveBeenRequestedWith
+  `expect('run.pivotal.io').toHaveBeenRequestedWith({method: 'POST', requestHeaders: 'let me in'})`
+
+### Math Matchers
+
+* toAlmostEqual
+  `expect(2.000001).toAlmostEqual(2, 4);`
+
+
+### React Matchers
+
+* toHaveBeenRenderedWithProps
+  `expect(MyComponent).toHaveBeenRenderedWithProps({expanded: true});`
--- tasks/publish.js
+++ tasks/publish.js
@@ -1,6 +1,39 @@
 const gulp = require('gulp');
-const {exec} = require('child_process');
+const {spawn} = require('child_process');
+const promisify = require('es6-promisify');
+const npm = require('npm');
+const glob = require('glob');
+const path = require('path');
 
-gulp.task('publish', ['build'], () => {
-  exec('lerna publish');
+const npmLoad = promisify(npm.load);
+
+
+gulp.task('lerna-publish', ['build'], (callback) => {
+  spawn('lerna', ['publish'], {stdio: 'inherit', env: process.env}).once('close', callback);
+});
+
+const addOwners = async function(packageNames) {
+  await npmLoad({});
+  const npmOwner = promisify(npm.commands.owner);
+
+  const owners = ['charleshansen', 'esharma', 'rdy'];
+  for (const owner of owners) {
+    for (const packageName of packageNames) {
+      try {
+        await npmOwner(['add', owner, packageName]);
+      } catch (e) {
+        console.log('failed adding owners', e);
+      }
+    }
+  }
+};
+
+gulp.task('publish', ['lerna-publish'], (done) => {
+  (async function() {
+    const packageNames = glob.sync('packages/*')
+      .map(filepath => path.basename(filepath));
+
+    await addOwners(packageNames);
+    done();
+  })();
 });
