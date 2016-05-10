319ff0f8843adae4e977f1438774c4e0bbf811ff
Mike Smykowski <msmykowski@pivotal.io>
2016-05-10 14:34:29 -0700

    toHaveBeenRenderedWithProps does not compare children
    Signed-off-by: Ryan Dy <rdy@pivotal.io>

--- packages/pivotal-js-jasmine-matchers/src/matchers/react_matchers.js
+++ packages/pivotal-js-jasmine-matchers/src/matchers/react_matchers.js
@@ -6,12 +6,13 @@ module.exports = {
         const displayExpected = jasmine.pp(expected);
 
         return actual.prototype.render.calls.all().reduce((result, renderCall) => {
-          result.pass = result.pass || util.equals(renderCall.object.props, jasmine.objectContaining(expected), customEqualityTesters);
+          const {children, ...propsWithoutChildren} = renderCall.object.props;
+          result.pass = result.pass || util.equals(propsWithoutChildren, jasmine.objectContaining(expected), customEqualityTesters);
 
           if (result.pass) {
             result.message = `Expected ${displayClass} not to have been rendered with props ${displayExpected}`;
           } else {
-            result.actualCallProps += jasmine.pp(renderCall.object.props) + '\n';
+            result.actualCallProps += jasmine.pp(propsWithoutChildren) + '\n';
             result.message = `Expected ${displayClass} to have been rendered with props ${displayExpected},\n but got ${result.actualCallProps}`;
           }
           return result;
