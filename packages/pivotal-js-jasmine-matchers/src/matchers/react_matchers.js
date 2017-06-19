63d1599922558325feec62933663a17370103d28
Ryan Dy <rdy@pivotal.io>
2017-06-19 13:06:45 -0700

    add toHaveBeenRenderedWithExactProps
    Signed-off-by: Trevor Whitney <twhitney@pivotal.io>

--- packages/pivotal-js-jasmine-matchers/src/matchers/react_matchers.js
+++ packages/pivotal-js-jasmine-matchers/src/matchers/react_matchers.js
@@ -23,5 +23,31 @@ module.exports = {
         });
       }
     };
+  },
+
+  toHaveBeenRenderedWithExactProps(util, customEqualityTesters) {
+    return {
+      compare(actual, expected) {
+        const displayClass = actual.displayName;
+        const displayExpected = jasmine.pp(expected);
+
+        return actual.prototype.render.calls.all().reduce((result, renderCall) => {
+          const {children, ...propsWithoutChildren} = renderCall.object.props;
+          result.pass = result.pass || util.equals(propsWithoutChildren, expected, customEqualityTesters);
+
+          if (result.pass) {
+            result.message = `Expected ${displayClass} not to have been rendered with props ${displayExpected}`;
+          } else {
+            result.actualCallProps += jasmine.pp(propsWithoutChildren) + '\n';
+            result.message = `Expected ${displayClass} to have been rendered with props ${displayExpected},\n but got ${result.actualCallProps}`;
+          }
+          return result;
+        }, {
+          pass: false,
+          message: `Expected ${displayClass} to have been rendered with props ${displayExpected}, but it was never rendered`,
+          actualCallProps: ''
+        });
+      }
+    };
   }
 };
